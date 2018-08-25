b83f4a06e3dc4183dc038e3fe16af3882874541f
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-08-25 12:11:29 +0530

    export mnemonic,keypair and wif from mnemonic

---
+++ src/mnemonic.js
@@ -0,0 +1,18 @@
+bitcoinjs = require("bitcoinjs-lib")
+
+bip39 = require("bip39")
+bitcoin = bitcoinjs
+
+function rng () { return Buffer.from('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz') }
+const keyPair = bitcoin.ECPair.makeRandom({ rng: rng })
+const wif = keyPair.toWIF()
+console.log('WIF:',wif);
+
+const mnemonic = bip39.entropyToMnemonic(rng())
+// console.log('mnemonic:',mnemonic, '\n');
+
+// console.log(bip39.mnemonicToEntropy(mnemonic));
+
+exports.mnemonic = mnemonic;
+exports.keyPair = keyPair;
+exports.wif = wif;
