c9dd86ee965d1bd1259d1a5b6954b0608607e281
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-08-25 16:16:42 +0530

    error from buffer; says input should not be a number; parsing secret share as buffer; need to convert string to buffer

--- src/split_share.js
+++ src/split_share.js
@@ -1,19 +1,35 @@
 secrets = require('secrets.js-grempe')
+createHash = require("create-hash")
+
+function checksum(input){
+  return createHash('sha256').update(Buffer.from(input)).digest()
+}
+
 
 var wif = 'L1Knwj9W3qK3qMKdTvmg3VfzUs3ij2LETTFhxza9LfD5dngnoLG1';
 // var key = wif.
 
-var key = secrets.random(128)
+var key = secrets.random(256)
 
-console.log(key);
+console.log('key',key);
 
 var shares_arr = secrets.share(key,5,3)
 
 console.log(shares_arr);
 
+function share_checksum(share_arr){
+  var checksumArray = [];
+  for (var i=0 ; i < shares_arr.length ; i++){
+    check = checksum(Buffer.from(i));
+    checksumArray.push(checks);
+  };
+  return checksumArray;
+}
+
+share_checksum(shares_arr)
 
 var reconstruct = secrets.combine(shares_arr.splice(0,3))
-console.log(reconstruct);
+console.log('reconstructed key', reconstruct);
 
 function verify(reconstruct, key){
   if(reconstruct == key){
@@ -24,3 +40,9 @@ function verify(reconstruct, key){
 }
 
 verify(key, reconstruct)
+
+
+module.exports = {
+  reconstruct,
+  verify
+}
